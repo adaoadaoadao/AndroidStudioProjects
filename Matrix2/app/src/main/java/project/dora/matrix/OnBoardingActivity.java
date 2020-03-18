8f63012a6d1261aee6055cf486bc8b336285ced7
adaoadaoadao <yww1996@126.com>
2020-03-18 18:01:15 -0400

    first commit

---
+++ Matrix2/app/src/main/java/project/dora/matrix/OnBoardingActivity.java
@@ -0,0 +1,34 @@
+package project.dora.matrix;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+
+        // setup viewpager and tablayout
+        viewPager = findViewById(R.id.viewpager);
+        OnBoardingPageAdapter onBoardingPageAdapter = new OnBoardingPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(onBoardingPageAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    // switch viewpage to #page
+    public void setCurrentPage(int page) {
+        viewPager.setCurrentItem(page);
+    }
+
+}
